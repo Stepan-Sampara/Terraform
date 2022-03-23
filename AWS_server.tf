e2c58f3fb96c90ec532e22f69a926597d9fff587
Stepan <stepansampara@gmail.com>
2022-03-23 14:47:28 -0700

    new wersion

--- AWS_server.tf
+++ AWS_server.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "Ubuntu" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.app.id}"]
   user_data = file("user_data.sh")
-  key_name  = "id_rsa"
+  key_name  = "ec2-key"
   tags = {
     Name = "Ubuntu_tomcat"
     Owner = "Stepan Sampara"
@@ -19,25 +19,12 @@ resource "aws_instance" "Amazon_Linux" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.app.id}"]
   user_data = file("CentOS.sh")
-  key_name  = "id_rsa"
+  key_name  = "ec2-key"
   tags = {
     NAME = "CentOS_Postgre" 
     Owner = "Stepan Sampara"
   }
-#  connection {
-#     type        = "ssh"
-#     host        = self.public_ip
-#     user        = "ubuntu"
-#     private_key = file("/home/step/.ssh/step_key")
-#     timeout     = "4m"
-#   }
-
 }
-
-   resource "aws_key_pair" "ssh-key" {
-    key_name   = "id_rsa"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDcmQW1thpKV/6wZcTmVKx00MyrZeEaUWHAghzGVmZAJgI0ME+jQ7Fl+G/ySv1vKAixHbcndCjMlXz0ZLiS6O8Lnpfok5/h6YFs6cueFBtK+Ups3ykQgTNb1+s2KKSopCHDGQARvAXsp/KeIyyiNCmbRZFWv+q04Db4NM/CjKy6bp3ZmECVv/0PuLoN8NIUWkeDRsOXyO0U29WhCNcZTLWRazrrVIRUWLy41Sby4gsVtcv/NKxsoqpKCT+T1OGV1CmbOFg6Z4ScN/2SyZydgxZ3Ugw66JGbN+0VNyC0S2pkHJvRaIYL1lRYFp8QsG6c21qTMme8DIdSjjnZTnONn7DPKnuQ1cgtjFXNZo+EIUrrikjiYGyZVOC1pbEWYo5Wu/OB0/gW9N0HKTtDlfjJmMR+uu4YxoZRIMKcSpM9KzbAxKirBueCJ3AGboVfOZkwgCajiuAYMe7M6LXLSSjWjttZF4K8JR8Z84vfezP9ye54sHKbhzJn/sNC/yejD9hFGwE="
-   }
 
 resource "aws_security_group" "app" {
   name        = "allow_tls"
